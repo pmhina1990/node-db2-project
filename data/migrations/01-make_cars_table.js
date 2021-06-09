9aa9df6db06eb33c16e70eda65e261d00afa8104
Peter Benjamin Mhina <peterbenjaminmhina@Peters-MacBook-Pro.local>
2021-06-08 20:37:27 -0700

    passed

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,22 @@
 // DO YOUR MAGIC
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments()
+
+        tbl.string('vin', 17).notNullable().unique()
+
+        tbl.string('make', 128).notNullable()
+
+        tbl.string('model', 150).notNullable()
+
+        tbl.integer('mileage').unsigned().notNullable()
+
+        tbl.string('title', 128)
+
+        tbl.string('transmission', 128)
+    })
+}
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars' )
+}
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
 // STRETCH
+const cars =[
+    {
+        vin: 'JA3AY11A82U020534',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '3GCEC14X66G218202',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 115000,
+        title: 'salvage',
+        
+    },
+    {
+        vin: '1JTJHA31U940036422',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+        
+    },
+]
+
+exports.seed = async function(knex) {
+   await knex('cars').truncate()
+   await knex('cars').insert(cars)
+    
+}
